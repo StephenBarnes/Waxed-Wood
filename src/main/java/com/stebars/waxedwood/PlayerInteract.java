97eeb9a922011ad73566e9bf3c1fc2899aee3b47
Stephen Barnes <stephen6174@gmail.com>
2021-10-23 05:03:27 -0500

    Waxing and stripping work, fireproof. Not using different textures yet, no recipes yet.

---
+++ src/main/java/com/stebars/waxedwood/PlayerInteract.java
@@ -0,0 +1,96 @@
+package com.stebars.waxedwood;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.particles.BlockParticleData;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.state.Property;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+@EventBusSubscriber
+public class PlayerInteract {
+	
+	@SubscribeEvent
+	public static void waxWood(final PlayerInteractEvent.RightClickBlock event) {
+		if (!(event.getItemStack().getItem() == Items.HONEYCOMB))
+			return;
+		BlockState state = event.getWorld().getBlockState(event.getPos());
+		Block block = state.getBlock();
+		Block newBlock = WaxedWoodMod.ID_TO_WAXED_BLOCK.get(block.getRegistryName());
+		if (newBlock == null)
+			return;
+		BlockPos pos = event.getPos();
+		Vector3d centerPos = Vector3d.atCenterOf(pos);
+		World world = event.getWorld();
+		
+		// Copy over properties, so we don't change eg rotation of stairs
+		BlockState newState = newBlock.defaultBlockState();
+		/*for (Entry<Property<?>, Comparable<?>> property: blockstate.getValues().entrySet()) {
+			replacementBlockState.setValue(property.getKey(), property.getValue());
+		} -- this doesn't work*/
+		for (Property property: WaxedWoodMod.BLOCK_PROPERTIES.get(newBlock)) {
+			newState = newState.setValue(property, state.getValue(property));
+		}
+
+		world.playSound(event.getPlayer(), pos, SoundEvents.HONEY_BLOCK_SLIDE, SoundCategory.BLOCKS, 1F, 1F); // TODO HONEY_BLOCK_PLACE?
+		world.setBlock(pos, newState, 3);
+		if (!event.getPlayer().isCreative())
+			event.getItemStack().shrink(1);
+
+		Random random = world.getRandom();
+        for(int i = 0; i < 7; ++i) {
+           world.addParticle(new BlockParticleData(ParticleTypes.BLOCK, Blocks.HONEY_BLOCK.defaultBlockState()),
+        		   centerPos.x + random.nextFloat() - .5,
+        		   centerPos.y + random.nextFloat() - .5,
+        		   centerPos.z + random.nextFloat() - .5,
+        		   0.0D, 0.0D, 0.0D);
+        }
+	}
+	
+	@SubscribeEvent
+	public static void stripBlock(final PlayerInteractEvent.RightClickBlock event) {
+		if (!event.getItemStack().getToolTypes().contains(ToolType.AXE))
+			return;
+		World world = event.getWorld();
+		BlockPos pos = event.getPos();
+		BlockState state = world.getBlockState(pos);
+		Block block = state.getBlock();
+		ResourceLocation newId = WaxedWoodMod.TOOL_MODIFIED.get(block);
+		if (newId == null)
+			return;
+		Block newBlock = ForgeRegistries.BLOCKS.getValue(newId);
+		BlockState newState =  newBlock.defaultBlockState();
+
+		// Copy properties, so we don't rotate logs
+		for (Property property: WaxedWoodMod.BLOCK_PROPERTIES.get(block)) {
+			newState = newState.setValue(property, state.getValue(property));
+		}
+		
+        PlayerEntity player = event.getPlayer();
+        world.playSound(player, pos, SoundEvents.AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        if (!world.isClientSide) {
+           world.setBlock(pos, newState, 11);
+           if (player != null) {
+        	   event.getItemStack().hurtAndBreak(1, player, (p_220040_1_) -> {
+                 p_220040_1_.broadcastBreakEvent(event.getHand());
+              });
+           }
+        }
+	}
+}
---
+++ src/main/java/com/stebars/waxedwood/WaxedWoodMod.java
@@ -0,0 +1,186 @@
+package com.stebars.waxedwood;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.FenceBlock;
+import net.minecraft.block.FenceGateBlock;
+import net.minecraft.block.RotatedPillarBlock;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.state.Property;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(WaxedWoodMod.MOD_ID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class WaxedWoodMod {
+	public final static String MOD_ID = "waxedwood";
+
+	public final static String[] VANILLA_WOOD = {
+			"oak",
+			"spruce",
+			"birch",
+			"jungle",
+			"acacia",
+			"dark_oak"
+	};
+	
+	public final static Property[] PLANKS_PROPERTIES = { };
+	public final static Property[] STAIRS_PROPERTIES = { BlockStateProperties.HORIZONTAL_FACING, BlockStateProperties.HALF, BlockStateProperties.STAIRS_SHAPE };
+	public final static Property[] SLAB_PROPERTIES = { BlockStateProperties.SLAB_TYPE };
+	public final static Property[] FENCE_PROPERTIES = {  };
+	public final static Property[] GATE_PROPERTIES = { BlockStateProperties.HORIZONTAL_FACING, BlockStateProperties.IN_WALL, BlockStateProperties.OPEN };
+	public final static Property[] LOG_PROPERTIES = { BlockStateProperties.AXIS };
+	public final static Property[] STRIPPED_LOG_PROPERTIES = { BlockStateProperties.AXIS };
+	public final static Property[] WOOD_PROPERTIES = { BlockStateProperties.AXIS };
+	public final static Property[] STRIPPED_WOOD_PROPERTIES = { BlockStateProperties.AXIS };
+
+	public static final Material waxed_WOOD_MATERIAL = (new Material.Builder(MaterialColor.WOOD)).build();
+
+	public static final Map<String, Block> PLANKS_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> STAIRS_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> SLAB_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> FENCE_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> GATE_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> LOG_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> STRIPPED_LOG_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> WOOD_BLOCKS = new HashMap<String, Block>();
+	public static final Map<String, Block> STRIPPED_WOOD_BLOCKS = new HashMap<String, Block>();
+	
+	public static final Map<ResourceLocation, Block> ID_TO_WAXED_BLOCK = new HashMap<ResourceLocation, Block>();
+	public static final Map<Block, Property[]> BLOCK_PROPERTIES = new HashMap<Block, Property[]>();
+	public static final Map<Block, ResourceLocation> TOOL_MODIFIED = new HashMap<Block, ResourceLocation>();
+
+
+	public WaxedWoodMod() {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+		for (String wood : VANILLA_WOOD) {
+			final Block planks_block = new Block(
+					AbstractBlock.Properties.of(waxed_WOOD_MATERIAL, MaterialColor.WOOD)
+					.strength(2.0F, 3.0F)
+					.harvestTool(ToolType.AXE)
+					.sound(SoundType.WOOD))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_" + wood + "_planks"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", wood + "_planks"), planks_block);
+			BLOCK_PROPERTIES.put(planks_block, PLANKS_PROPERTIES);
+			PLANKS_BLOCKS.put(wood, planks_block);
+
+			@SuppressWarnings("deprecation") // vanilla code uses StairsBlock
+			final Block stairs_block = new StairsBlock(
+					planks_block.defaultBlockState(),
+					AbstractBlock.Properties.copy(planks_block))
+			.setRegistryName(new ResourceLocation(MOD_ID, "waxed_" + wood + "_stairs"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", wood + "_stairs"), stairs_block);
+			BLOCK_PROPERTIES.put(stairs_block, STAIRS_PROPERTIES);
+			STAIRS_BLOCKS.put(wood, stairs_block);
+
+			final Block slab_block = new SlabBlock(
+					AbstractBlock.Properties.copy(planks_block))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_" + wood + "_slab"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", wood + "_slab"), slab_block);
+			BLOCK_PROPERTIES.put(slab_block, SLAB_PROPERTIES);
+			SLAB_BLOCKS.put(wood, slab_block);
+
+			final Block gate_block = new FenceGateBlock(
+					AbstractBlock.Properties.copy(planks_block))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_" + wood + "_fence_gate"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", wood + "_fence_gate"), gate_block);
+			BLOCK_PROPERTIES.put(gate_block, GATE_PROPERTIES);
+			GATE_BLOCKS.put(wood, gate_block);
+
+			final Block fence_block = new FenceBlock(
+					AbstractBlock.Properties.copy(planks_block))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_" + wood + "_fence"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", wood + "_fence"), fence_block);
+			BLOCK_PROPERTIES.put(fence_block, FENCE_PROPERTIES);
+			FENCE_BLOCKS.put(wood, fence_block);
+
+			final Block log_block = new RotatedPillarBlock(AbstractBlock.Properties.of(waxed_WOOD_MATERIAL,
+					(p_235431_2_) -> {
+						return p_235431_2_.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? MaterialColor.WOOD : MaterialColor.PODZOL;
+						// TODO the colors are different for different types of wood
+					}).strength(2.0F).sound(SoundType.WOOD)
+					.harvestTool(ToolType.AXE))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_" + wood + "_log"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", wood + "_log"), log_block);
+			BLOCK_PROPERTIES.put(log_block, LOG_PROPERTIES);
+			LOG_BLOCKS.put(wood, log_block);
+
+			final Block stripped_log_block = new RotatedPillarBlock(AbstractBlock.Properties.of(waxed_WOOD_MATERIAL,
+					(p_235431_2_) -> {
+						return p_235431_2_.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? MaterialColor.PODZOL : MaterialColor.PODZOL;
+						// TODO the colors are different for different types of wood
+					}).strength(2.0F).sound(SoundType.WOOD)
+					.harvestTool(ToolType.AXE))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_stripped_" + wood + "_log"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", "stripped_" + wood + "_log"), stripped_log_block);
+			BLOCK_PROPERTIES.put(stripped_log_block, STRIPPED_LOG_PROPERTIES);
+			STRIPPED_LOG_BLOCKS.put(wood, stripped_log_block);
+			
+			final Block wood_block = new RotatedPillarBlock(
+					AbstractBlock.Properties.copy(planks_block)
+					.strength(2.0F))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_" + wood + "_wood"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", wood + "_wood"), wood_block);
+			BLOCK_PROPERTIES.put(wood_block, WOOD_PROPERTIES);
+			WOOD_BLOCKS.put(wood, wood_block);
+
+			final Block stripped_wood_block = new RotatedPillarBlock(
+					AbstractBlock.Properties.copy(planks_block)
+					.strength(2.0F))
+					.setRegistryName(new ResourceLocation(MOD_ID, "waxed_stripped_" + wood + "_wood"));
+			ID_TO_WAXED_BLOCK.put(new ResourceLocation("minecraft", "stripped_" + wood + "_wood"), stripped_wood_block);
+			BLOCK_PROPERTIES.put(stripped_wood_block, STRIPPED_WOOD_PROPERTIES);
+			STRIPPED_WOOD_BLOCKS.put(wood, stripped_wood_block);
+
+			TOOL_MODIFIED.put(log_block, new ResourceLocation("minecraft", "stripped_" + wood + "_log"));
+			TOOL_MODIFIED.put(wood_block, new ResourceLocation("minecraft", "stripped_" + wood + "_wood"));
+
+			event.getRegistry().registerAll(planks_block, stairs_block, slab_block, gate_block, fence_block,
+					log_block, stripped_log_block, wood_block, stripped_wood_block);
+		}
+	}
+
+	private static Item makeBlockItem(String id, Block block) {
+		return new BlockItem(block,
+				new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS))
+				.setRegistryName(new ResourceLocation(MOD_ID, id));
+	}
+
+	@SubscribeEvent
+	public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+		for (String wood : VANILLA_WOOD) {
+			event.getRegistry().registerAll(
+					makeBlockItem("waxed_" + wood + "_planks", PLANKS_BLOCKS.get(wood)),
+					makeBlockItem("waxed_" + wood + "_stairs", STAIRS_BLOCKS.get(wood)),
+					makeBlockItem("waxed_" + wood + "_slab", SLAB_BLOCKS.get(wood)),
+					makeBlockItem("waxed_" + wood + "_fence_gate", GATE_BLOCKS.get(wood)),
+					makeBlockItem("waxed_" + wood + "_fence", FENCE_BLOCKS.get(wood)),
+					makeBlockItem("waxed_" + wood + "_log", LOG_BLOCKS.get(wood)),
+					makeBlockItem("waxed_stripped_" + wood + "_log", STRIPPED_LOG_BLOCKS.get(wood)),
+					makeBlockItem("waxed_" + wood + "_wood", WOOD_BLOCKS.get(wood)),
+					makeBlockItem("waxed_stripped_" + wood + "_wood", STRIPPED_WOOD_BLOCKS.get(wood))
+					);
+		}
+	}
+}
